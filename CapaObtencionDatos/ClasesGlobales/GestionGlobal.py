d8651748e145bfd37d5bb9937d02f3b374152567
SebastianMaldonado <98917583+SebastianMaldonado@users.noreply.github.com>
2023-11-04 13:29:29 -0500

    Add files via upload

---
+++ CapaObtencionDatos/ClasesGlobales/GestionGlobal.py
@@ -0,0 +1,43 @@
+import sys, os
+sys.path.insert(0, f'{os.path.dirname(os.path.dirname(__file__))}')
+
+from ConversionDatos.ConversionDatos import ConversionDatos
+from ExtraccionDatos.ExtraccionDatos import ExtraccionDatos
+
+'''
+|====================================================================|
+*                       |Gestión Global|         
+* Descripción:                                                        
+*   Archivo en el que se tiene acceso a todas las clases del programa
+*   con el fin de ejecutar procesos conjuntos entre las diferentes
+*   capas del sistema
+|====================================================================|
+'''
+
+extraccion = ExtraccionDatos()
+conversor = ConversionDatos()
+
+# Descargar todos los Datasets ingresados en las Entradas de la Capa de Extracción de Datos
+#   -> Artemisa-PC-01/Artemisa-2.0/CapaObtencionDatos/ExtraccionDatos/Entradas de Capa/ListaDatasets.txt
+res_descarga = extraccion.descargarListaDatasets()
+print(res_descarga)
+
+'''
+ Para encontrar el resultado de cada operación de descarga consultar:
+   -> Artemisa-PC-01/Artemisa-2.0/CapaObtencionDatos/ExtraccionDatos/Historial/Registro_descargas.txt
+'''
+
+# Analizar preliminarmente los datasets descargados para registrar las especies
+#   -> Artemisa-PC-01/Artemisa-2.0/CapaObtencionDatos/ExtraccionDatos/Entradas de Capa/ListaConsultas.txt
+res_registro = conversor.registrarEspeciesConsultas()
+print(res_registro)
+
+# Consultar todos los nombres de las especies encontradas en los datasets, los resultados se guardarán en
+#   -> Artemisa-PC-01/Artemisa-2.0/CapaObtencionDatos/ExtraccionDatos/Entradas de Capa/ListaDatasets.txt
+res_consultas = extraccion.registrarDatasetsIncidencias()
+print(res_consultas)
+
+'''
+ Para encontrar el resultado de cada operación de descarga consultar:
+   -> Artemisa-PC-01/Artemisa-2.0/CapaObtencionDatos/ExtraccionDatos/Historial/Registro_consultas.txt
+'''
---
+++ CapaObtencionDatos/ClasesGlobales/VariablesFuncionamiento.py
@@ -0,0 +1,33 @@
+from abc import ABC
+import os
+
+class VariablesFuncionamiento (ABC):
+
+    def __init__ (self):
+ 
+        #Variables de control de errores
+        self.exito = 'EXITO'
+        self.error = 'ERROR'
+
+        #Variables de Validación de Usuario de GBIF
+        self.GBIF_USER = 'artemisa'
+        self.GBIF_PWD = 'Artemisa1945'
+        self.GBIF_EMAIL = 'artemisa1945@outlook.com'
+
+        #Variables de Validación de Usuario para MySQL
+        self.SQL_HOST = ''
+        self.SQL_USER = 'artemisa'
+        self.SQL_PWD = 'Datachallenge2023'
+
+        self.CLAVE_SERVIDOR_SQL = 'Driver={SQL Server};Server=tcp:artemisa-2.database.windows.net,1433;Database=artemisa-base-datos;Uid=azureuser;Pwd={Datachallenge2023};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+
+        #Variables de manejo de direcciones
+        self.direccion_carpeta = f'{os.path.dirname(os.path.dirname(__file__))}'
+        
+        self.direccion_descarga = f"{self.direccion_carpeta}/ExtraccionDatos/Dataset Descargados"
+        self.direccion_descompresion = f"{self.direccion_carpeta}/ExtraccionDatos/Dataset Descomprimidos"
+        self.direccion_bd_sin_procesar = f'{self.direccion_carpeta}/ConversionDatos/Base de Datos - Sin Procesar'
+        self.entradas_extraccion_datos = f'{self.direccion_carpeta}/ExtraccionDatos/Entradas de Capa'
+        self.registro_extraccion_datos = f'{self.direccion_carpeta}/ExtraccionDatos/Historial'
+        
+        self.direccion_datos_globales = f'{self.direccion_carpeta}/DatosGlobales'
